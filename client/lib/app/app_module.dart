6cce0cdb058f47d4d1a78328b94b4d151ccda85d
tiago <tiago.moreira@timebrisa.com.br>
2022-07-20 16:59:27 -0300

    fix: :bug: Corrigindo conflitos

---
+++ client/lib/app/app_module.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+
+import 'modules/home/home_module.dart';
+
+import 'splash_page.dart';
+
+class AppModule extends Module {
+  @override
+  final List<Bind> binds = [];
+
+  @override
+  final List<ModularRoute> routes = [
+    // ModuleRoute(Modular.initialRoute, module: HomeModule()),
+    ChildRoute('/', child: ((context, args) => const SplashPage())),
+    ModuleRoute('/home', module: HomeModule()),
+    WildcardRoute(
+        child: (_, __) => const Scaffold(
+              body: Center(child: Text("A página não existe!")),
+            )),
+  ];
+}
